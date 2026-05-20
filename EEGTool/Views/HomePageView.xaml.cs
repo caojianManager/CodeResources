7c24b30cb3b5be478fb583898e3151e8b7f0e0ce
caojian <caojianmanager@163.com>
2026-05-20 15:21:31 +0800

    搭建脑电图机工具

--- EEGTool/Views/HomePageView.xaml.cs
+++ EEGTool/Views/HomePageView.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using EegAcquisitionSystem;
 using FrameWork.View;
+using MahApps.Metro.IconPacks;
+using OpenTK.Windowing.Common;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using WpfAnimatedGif;
+using MouseButtonEventArgs = System.Windows.Input.MouseButtonEventArgs;
+using WindowState = System.Windows.WindowState;
 
 namespace EEGTool.Views
 {
@@ -23,10 +20,90 @@ namespace EEGTool.Views
         public HomePageView()
         {
             InitializeComponent();
+            StateChanged += (_, _) => UpdateWindowChromeState();
+            Loaded += HomePageView_Loaded;
+            UpdateWindowChromeState();
+        }
+
+        private void HomePageView_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadGifStartupAni();
+        }
+
+        private void LoadGifStartupAni()
+        {
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri("pack://application:,,,/EEGTool;component/Resources/images/OpenBCI-LoadingGIF-blue-256.gif");
+            image.EndInit();
+            ImageBehavior.SetAnimatedSource(StartupAniImage, image);
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeRestore_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleMaximizeRestore();
+        }
+
+        private void ToggleMaximizeRestore()
+        {
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+
+            UpdateWindowChromeState();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            App.ExitEvt?.Invoke();
+            Application.Current.Shutdown();
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void UpdateWindowChromeState()
+        {
+            bool isMaximized = WindowState == WindowState.Maximized;
+            MaxIcon.Kind = isMaximized
+                ? PackIconMaterialKind.WindowRestore
+                : PackIconMaterialKind.WindowMaximize;
+        }
+
+        private void TryDragWindow()
+        {
+            try
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    Point localPoint = Mouse.GetPosition(this);
+                    Point screenPoint = PointToScreen(localPoint);
+                    double widthRatio = ActualWidth > 0 ? localPoint.X / ActualWidth : 0.5;
+
+                    WindowState = WindowState.Normal;
+                    Left = screenPoint.X - (RestoreBounds.Width * widthRatio);
+                    Top = Math.Max(0, screenPoint.Y - 16);
+                    UpdateWindowChromeState();
+                }
+
+                DragMove();
+            }
+            catch
+            {
+            }
         }
 
         private void OuterBorder_OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
+            bool isMaximized = WindowState == WindowState.Maximized;
+            double radius = isMaximized ? 0 : 5;
+
+            OuterBorder.CornerRadius = new CornerRadius(radius);
+            OuterBorderClip.RadiusX = radius;
+            OuterBorderClip.RadiusY = radius;
             OuterBorderClip.Rect = new Rect(0, 0, OuterBorder.ActualWidth, OuterBorder.ActualHeight);
         }
     }
