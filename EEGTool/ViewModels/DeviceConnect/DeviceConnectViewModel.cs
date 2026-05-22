e2f1362ad951ecab534d66d6eccab260d3aa8be3
caojian <caojianmanager@163.com>
2026-05-22 16:14:00 +0800

    开发蓝牙设备连接

--- EEGTool/ViewModels/DeviceConnect/DeviceConnectViewModel.cs
+++ EEGTool/ViewModels/DeviceConnect/DeviceConnectViewModel.cs
@@ -3,13 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.ApplicationModel.VoiceCommands;
+using Windows.Media.Audio;
+using BLETool;
 using FrameWork.MVVM;
+using Framework.MVVM.Commands;
 
 namespace EEGTool.ViewModels.DeviceConnect
 {
     public class DeviceConnectViewModel:BindableBase
     {
 
+        private BleManager _ble;
 
+        public ICommand? ScanDeviceCommand { get; set; }
+
+
+        private DeviceConnectViewModel()
+        {
+            Config();
+        }
+
+        private void Config()
+        {
+            _ble = BleToolKit.Shared;
+            _ble.DeviceDiscovered += (_, d) => { DeviceDiscovered(d); };
+            _ble.DeviceUpdated += (_, d) => { DeviceUpdated(d); };
+
+            ScanDeviceCommand = new RelayCommand((o) =>
+            {
+                _ = ScanBTDevice();
+            });
+        }
+
+        private void DeviceDiscovered(BleDeviceInfo deviceInfo)
+        {
+            Console.WriteLine($"{deviceInfo.DisplayName} {deviceInfo.Address} {deviceInfo.Rssi}");
+        }
+
+        private void DeviceUpdated(BleDeviceInfo deviceInfo)
+        {
+            Console.WriteLine($"更新: {deviceInfo.DisplayName} {deviceInfo.Rssi}");
+        }
+
+
+        /// <summary>
+        /// 扫描蓝牙设备
+        /// </summary>
+        /// <returns></returns>
+        private async Task ScanBTDevice()
+        {
+            if(_ble == null)
+                return;
+            _ble.StartScan();
+            await Task.Delay(500);
+            _ble.StopScan();
+
+
+            //获取扫描结果
+            var devices = _ble.GetDiscoveredDevices();
+
+            //按照蓝牙名称过滤设备
+            devices = devices.Where(d => d.Name.StartsWith("16CH_", StringComparison.OrdinalIgnoreCase))
+                .ToList();
+        }
     }
 }
