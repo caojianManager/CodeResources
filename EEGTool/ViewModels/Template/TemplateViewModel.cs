2cc147fc150098ddc45cf748338fbe03e2ebf7b8
caojian <caojianmanager@163.com>
2026-05-28 10:39:54 +0800

    开发新建模板弹窗

--- EEGTool/ViewModels/Template/TemplateViewModel.cs
+++ EEGTool/ViewModels/Template/TemplateViewModel.cs
@@ -3,16 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using FrameWork.MVVM;
+using Framework.MVVM.Commands;
 
 namespace EEGTool.ViewModels.Template
 {
     public class TemplateViewModel : BindableBase
     {
+        private bool _isShowCreateTemplateWindow = false;
+
+        public bool IsShowCreateTemplateWindow
+        {
+            get => _isShowCreateTemplateWindow;
+            set => SetProperty(ref _isShowCreateTemplateWindow, value);
+        }
+
+        public ICommand? CreateTemplateCommand { get; set; }
+        public ICommand? CancelCreateCommand { get; set; }
+        public ICommand? SureCreateTemplateCommand { get; set; }
 
         public TemplateViewModel()
         {
-
+            Config();
         }
+
+        private void Config()
+        {
+            CreateTemplateCommand = new RelayCommand((o) =>
+            {
+                CreateTemplate();
+            });
+
+            SureCreateTemplateCommand = new RelayCommand((o) =>
+            {
+                SureCreateTemplate();
+            });
+
+            CancelCreateCommand = new RelayCommand((o) =>
+            {
+                IsShowCreateTemplateWindow = false;
+            });
+        }
+
+        private void SureCreateTemplate()
+        {
+            IsShowCreateTemplateWindow = false;
+        }
+
+
+        private void CreateTemplate()
+        {
+            IsShowCreateTemplateWindow = true;
+        }
+
+
     }
 }
