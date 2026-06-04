a42a8a78a16724b7135191a98eb44691841a8bbd
caojian <caojianmanager@163.com>
2026-06-04 15:43:54 +0800

    持久化存储采集配置

--- EEGTool/FrameWork/Common/Config.cs
+++ EEGTool/FrameWork/Common/Config.cs
@@ -23,49 +23,12 @@ namespace FrameWork.Common
         }
 
 
-        public double Impedance_BandPassLow = 21.0;
-        public double Impedance_BandPassHeigh = 41.0;
-        public double AA = 10e-9;
-        public int series_resistor_kohm = 10;
+        /*持久化信息存储*/
 
-
-        public bool IsUseProxy = false;
-        public string ProxyAddress = "http://192.168.100.33:9090";
-        public string HTTP_IP = "192.168.1.104";
-        public int HTTP_PORT = 49090;
-        public double ReferenceVoltage = 4.5;
-
-        public string InitSampleRate = "250Hz";            //采样率
-        public int SampleRate = 500;            //采样率
+        /*---------采集配置相关的属性-------------*/
         public string CollectionSelectedSampleRate = "250Hz";
         public string CollectionSelectedTemplate = string.Empty;
         public bool CollectionIsVideoRecordYes = false;
-        public int DefaultTimeWindowSec = 20;   //默认时间窗口大小
-        public int ChannelCount = 16;           //通道数量
-        public int GainNum = 6;                 //放大倍数
-        public bool IsEegAuto = true;          //Eeg时序图是否开启自动纵轴
-
-        //滤波
-        public bool IsOpenBandPass = false;     //带通开关
-        public bool IsOpenNotchFilter = false;  //陷波开关
-        public double BandPassLow = 0.5;
-        public double BandPassHeigh = 50;
-        public int NotchFilterHz = 0;           // 0-None,1-50Hz 2-60Hz
-
-
-
-        //设置
-        public int ImpedanceMaxValue = 0;
-        public int ImpedanceMinValue = 0;
-        public bool IsCanImpeDance = false;
-        //医院
-        public string HospitalName = string.Empty;
-        public string HospitalAddress = string.Empty;
-        public string HospitalPhone = string.Empty;
-        public string HospitalIntroduce = string.Empty;
-
-
-
 
         public void Init()
         {
