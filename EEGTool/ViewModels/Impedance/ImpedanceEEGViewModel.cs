edb8fcb50043b7622530fa2aa4418191c05b2f91
caojian <caojianmanager@163.com>
2026-06-12 17:50:55 +0800

    创建阻抗监测页面

---
+++ EEGTool/ViewModels/Impedance/ImpedanceEEGViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrameWork.MVVM;
+
+namespace EEGTool.ViewModels.Impedance
+{
+    public class ImpedanceEEGViewModel : BindableBase
+    {
+
+        public ImpedanceEEGViewModel()
+        {
+
+        }
+    }
+}
---
+++ EEGTool/ViewModels/Impedance/ImpedanceListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEGTool.ViewModels.Impedance
+{
+    internal class ImpedanceListViewModel
+    {
+    }
+}
