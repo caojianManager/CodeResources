540b57c22ecdbc79bb2056f1b9702d8dc6fbff72
caojian <caojianmanager@163.com>
2026-06-24 08:39:05 +0800

    监听

---
+++ EEGTool/ViewModels/Collection/BandPowerMonitorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrameWork.MVVM;
+
+namespace EEGTool.ViewModels.Collection
+{
+    public class BandPowerMonitorViewModel : BindableBase
+    {
+
+        public BandPowerMonitorViewModel()
+        {
+
+        }
+    }
+}
