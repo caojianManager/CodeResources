c7de5dd05ca95a0183576d7f55b1093b028fa6c3
caojian <caojianmanager@163.com>
2026-08-19 13:48:58 +0800

    创建nRF 52840接入API RJCP 窗口读写API

---
+++ EEGTool/FrameWork/NRF52840/CDCErrorCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainZoneMultichannel.FrameWork.NRF52840
+{
+    internal class CDCErrorCode
+    {
+    }
+}
