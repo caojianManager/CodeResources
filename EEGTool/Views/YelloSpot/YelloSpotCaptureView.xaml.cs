2f111afe22ddd19661c9746607b0cc391b6ab2d3
caojian <caojianmanager@163.com>
2026-08-24 17:54:20 +0800

    创建顶点信息和索引

--- EEGTool/Views/YelloSpot/YelloSpotCaptureView.xaml.cs
+++ EEGTool/Views/YelloSpot/YelloSpotCaptureView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
 
 namespace EEGTool.Views.YelloSpot
 {
@@ -20,6 +22,26 @@ namespace EEGTool.Views.YelloSpot
     /// </summary>
     public partial class YelloSpotCaptureView : UserControl
     {
+        //1.顶点数据:矩形屏幕,顶点数据/索引
+        private float[] vertices = new float[]
+        {
+            0,0,0,
+            0,1,0,
+            1,1,0,
+            1,0,0,
+        };
+        private int[] indices = new int[]
+        {
+            0,1,2,
+            0,2,3,
+        };
+
+        private int _vao, _vbo, _ebo, _shaderProgram; //VAO,VBO,EBO,Shader
+        private int _modelLoc, _viewLoc, _projLoc;    //M-V-P 模型-观察-投影矩阵的句柄
+        private int _indexCount;                      //索引的数量
+        private bool _glResourcesInitialized = false; //资源是否进行了初始化
+
+
         public YelloSpotCaptureView()
         {
             InitializeComponent();
@@ -30,9 +52,27 @@ namespace EEGTool.Views.YelloSpot
         {
         }
 
+        private void InitGLResources()
+        {
+            _indexCount = indices.Length;
+
+            _vao = GL.GenVertexArray();
+            _vbo = GL.GenBuffer();
+            _ebo = GL.GenBuffer();
+
+            GL.BindVertexArray(_vao);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices,BufferUsage.StaticDraw);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(int), indices, BufferUsage.StaticDraw);
+        }
+
         //Step 3:渲染帧
         private void OpenTkControl_OnRender(TimeSpan obj)
         {
+
         }
     }
 }
