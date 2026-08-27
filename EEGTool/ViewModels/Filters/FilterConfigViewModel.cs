0958485eefc5a8fe236e1de0c96136ea09632575
caojian <caojianmanager@163.com>
2026-08-27 15:19:12 +0800

    创建滤波器配置窗口

---
+++ EEGTool/ViewModels/Filters/FilterConfigViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FrameWork.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEGTool.ViewModels.Filters
+{
+    public class FilterConfigViewModel : BindableBase
+    {
+
+        public FilterConfigViewModel() {
+            
+        }
+    }
+}
